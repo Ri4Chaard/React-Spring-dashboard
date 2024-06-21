b9ed2b7322040bbbb0fac43a1b6a0265ef9d4cd8
Richard <yarik271202@gmail.com>
2024-06-21 09:21:53 +0300

    Get by id and delete by id implemented

--- src/main/java/myjavaproject/apis/contoller/MovieController.java
+++ src/main/java/myjavaproject/apis/contoller/MovieController.java
@@ -1,14 +1,17 @@
 package myjavaproject.apis.contoller;
 
+import com.mongodb.client.result.DeleteResult;
 import jakarta.validation.Valid;
 import myjavaproject.apis.entity.Movie;
 import myjavaproject.apis.service.MovieService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/movies")
@@ -19,8 +22,16 @@ public class MovieController {
     public ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id){
+        return new ResponseEntity<>(movieService.singleMovie(id), HttpStatus.OK);
+    }
     @PostMapping
     public ResponseEntity<Movie> createMovie(@Valid @RequestBody Movie movie){
         return new ResponseEntity<>(movieService.createMovie(movie.getTitle(), movie.getGenres(), movie.getVisits()), HttpStatus.CREATED);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<DeleteResult> deleteMovie(@PathVariable ObjectId id){
+        return new ResponseEntity<>(movieService.deleteMovie(id), HttpStatus.OK);
     }
 }
--- src/main/java/myjavaproject/apis/service/MovieService.java
+++ src/main/java/myjavaproject/apis/service/MovieService.java
@@ -1,12 +1,17 @@
 package myjavaproject.apis.service;
 
+import com.mongodb.client.result.DeleteResult;
 import myjavaproject.apis.entity.Movie;
 import myjavaproject.apis.repository.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -19,8 +24,16 @@ public class MovieService {
         return movieRepository.findAll();
     }
 
+    public Optional<Movie> singleMovie(ObjectId id){
+        return movieRepository.findById(id);
+    }
+
     public Movie createMovie(String title, List<String> genres, List<Object> visits ){
         return mongoTemplate.insert(new Movie(title, genres, visits));
+    }
+
+    public DeleteResult deleteMovie(ObjectId id){
+        return mongoTemplate.remove(new Query((Criteria.where("_id").is(id))), Movie.class);
     }
 
 }
